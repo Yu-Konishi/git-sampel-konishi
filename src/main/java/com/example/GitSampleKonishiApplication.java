6c7cf9e635955368eb29a83df2e9d70ba255c0f3
Yu-Konishi <yu.konishi@rakus.co.jp>
2020-06-16 11:33:49 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleKonishiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKonishiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKonishiApplication.class, args);
+	}
+
+}
